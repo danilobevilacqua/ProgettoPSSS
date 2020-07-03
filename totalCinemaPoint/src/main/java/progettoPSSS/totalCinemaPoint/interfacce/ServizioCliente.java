cbbc7366b8529a358b65cff33f0492b001743bb7
Enzo Troisi <milanista957@gmail.com>
2020-07-03 20:03:56 +0200

    Fix del codice

--- totalCinemaPoint/src/main/java/progettoPSSS/totalCinemaPoint/interfacce/ServizioCliente.java
+++ totalCinemaPoint/src/main/java/progettoPSSS/totalCinemaPoint/interfacce/ServizioCliente.java
@@ -2,11 +2,6 @@ package progettoPSSS.totalCinemaPoint.interfacce;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.List;
-
-import progettoPSSS.totalCinemaPoint.server.entity.Film;
-import progettoPSSS.totalCinemaPoint.server.entity.Posto;
-import progettoPSSS.totalCinemaPoint.server.entity.Spettacolo;
 
 public interface ServizioCliente extends Remote {
 	String logIn(String username, String password) throws RemoteException;
